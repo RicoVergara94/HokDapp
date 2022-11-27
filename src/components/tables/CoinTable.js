4b4e677aa0cf81c01bad9a900f0dbea4cf63e6a6
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-11-27 05:19:21 -0500

    added table from antd library

--- src/components/tables/CoinTable.js
+++ src/components/tables/CoinTable.js
@@ -1,29 +1,5 @@
 import "./styles.css";
+import { Table } from "antd";
 export default function CoinTable() {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Icon</th>
-          <th>Address</th>
-          <th>Token</th>
-          <th>Balance</th>
-          <th>USD</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-          <td>1</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+  return <Table />;
 }
